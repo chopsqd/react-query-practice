c1721862b591f0e2a52030a86964b01f49e4c3c0
chopsqd <007micle007@gmail.com>
2025-10-27 02:01:12 +0300

    Implemented offline mode using PWA and Persistent storage

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,20 +1,40 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { App } from "./app.tsx";
 import "./index.css";
 import { queryClient } from "../shared/api/query-client.ts";
 import { store } from "../shared/store/redux.ts";
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
+import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
+import { onlineManager } from "@tanstack/react-query";
+
+onlineManager.setOnline(navigator.onLine)
+
+const persister = createSyncStoragePersister({
+  storage: window.localStorage
+});
+
+// Persist storage - кэши из запросов сохраняются в localStorage
+// Если зайти в offline mode - данные будут из localStorage
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
+    <PersistQueryClientProvider
+      client={queryClient}
+      persistOptions={{ persister }}
+      onSuccess={() => {
+        // Возобновить мутации после первоначального восстановления из localStorage
+        queryClient.resumePausedMutations().then(() => {
+          queryClient.invalidateQueries();
+        });
+      }}
+    >
       <Provider store={store}>
         <App />
       </Provider>
       <ReactQueryDevtools />
-    </QueryClientProvider>
+    </PersistQueryClientProvider>
   </StrictMode>
 );
--- src/shared/api/query-client.ts
+++ src/shared/api/query-client.ts
@@ -3,7 +3,8 @@ import { QueryClient } from "@tanstack/react-query";
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 1 * 60 * 1000
+      staleTime: 1 * 60 * 1000,
+      gcTime: 24 * 60 * 60 * 1000 // часы * минуты * секунды * мс
       // stale time - время, в течение которого данные считаются актуальными
       // gc time - время, через которое неиспользуемые данные удаляются из кеша
     }
